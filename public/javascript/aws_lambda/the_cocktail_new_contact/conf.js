135dc574a07119d794c9b16bb3077305cd223cf1
Fernando Garcia Samblas <fernando.garcia@the-cocktail.com>
2016-06-08 11:47:12 +0200

    restore TCK Ventures email after testing new param

--- public/javascript/aws_lambda/the_cocktail_new_contact/conf.js
+++ public/javascript/aws_lambda/the_cocktail_new_contact/conf.js
@@ -6,8 +6,7 @@ module.exports = {
       emailSubject: "New contact in The Cocktail Website"
     },
     'thecocktailventures.com': {
-//      emailAddress: "TckVentures Website <ventures@the-cocktail.com>",
-      emailAddress: "The Cocktail Site <fernando.garcia@the-cocktail.com>",
+      emailAddress: "TckVentures Website <ventures@the-cocktail.com>",
       emailSubject: "New contact with The Cocktail Ventures"
     }
   }
